2eb112472c2b04f6fa2f56b2b066ff655a733ac3
MisterMeldgaard <rameldgaard@gmail.com>
2022-02-16 10:35:55 +0100

    Fixed Package problems

---
+++ sudoku/SudokuModel.java
@@ -0,0 +1,34 @@
+package sudoku;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class SudokuModel {
+    public SudokuModel (File file){
+        Scanner scanner;
+		try {
+			scanner = new Scanner(file);
+			String setup = scanner.next();
+			int n = Character.getNumericValue(setup.charAt(0));
+			int k = Character.getNumericValue(setup.charAt(2));
+
+			int[][] sudoku = new int[n*n][n*n];
+	    	for (int i = 0; i< n*n; i++) {
+	        String line = scanner.next();
+	    		for(int j = 0; j < n*n; j++) {
+	    			if(line.charAt(j*2)=='.') {
+	    				sudoku[i][j]=0;
+	    			} else {
+	    				sudoku[i][j]=Character.getNumericValue(line.charAt(j*2));
+	    			}
+	    			System.out.print(sudoku[i][j]);
+	    		}
+	    		System.out.println();
+	    	}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+}
