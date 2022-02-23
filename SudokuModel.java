0d2340bc933d388d0aa57dc5291afd7afaa6dcbd
FrederikBukHenriksen <62750296+FrederikBukHenriksen@users.noreply.github.com>
2022-02-23 15:39:39 +0100

    funktionalitet i Model

--- SudokuModel.java
+++ SudokuModel.java
@@ -1,18 +1,28 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import javax.swing.JToggleButton;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class SudokuModel {
 
 	int[][] sudoku = new int[0][0];
+	ArrayList<ArrayList<int[][]>> allSections = new ArrayList<>();
 
-	public SudokuModel(File file) {
+	int n, k;
+
+	public SudokuModel() {
+	}
+
+	public void loadASCIIFile(File file) {
 		Scanner scanner;
 		try {
 			scanner = new Scanner(file);
 			String setup = scanner.next();
-			int n = Character.getNumericValue(setup.charAt(0));
-			int k = Character.getNumericValue(setup.charAt(2));
+			n = Character.getNumericValue(setup.charAt(0));
+			k = Character.getNumericValue(setup.charAt(2));
 
 			sudoku = new int[n * n][n * n];
 			for (int i = 0; i < n * n; i++) {
@@ -31,6 +41,52 @@ public class SudokuModel {
 		}
 	}
 
+	public void createSudokuSections() {
+		// Controls the shifting between sections.
+		for (int x = 0; x < 3; x++) {
+			ArrayList<int[][]> rowSections = new ArrayList<>();
+			for (int i = 0; i < 3; i++) {
+
+				// Runs through a single section.
+				int[][] section = new int[3][3];
+				for (int l = 0; l < 3; l++) {
+					for (int m = 0; m < 3; m++) {
+						section[l][m] = sudoku[l + x * 3][m + i * 3];
+					}
+				}
+				rowSections.add(section);
+			}
+			allSections.add(rowSections);
+		}
+	}
+
+	public ArrayList<ArrayList<int[][]>> getSections() {
+		return allSections;
+	}
+
+	public int[][] getSection(int x, int y) {
+		return allSections.get(x).get(y);
+	}
+
+	/**
+	 * @param coordinates of a section
+	 * @return a primitive array containing the numbers of the section
+	 */
+	public int[] getSectionList(int x, int y) {
+		int[][] section = allSections.get(x).get(y);
+
+		ArrayList<Integer> temp = new ArrayList<>();
+		for (int i = 0; i < (section.length); i++) {
+			for (int j = 0; j < section.length; j++) {
+				temp.add(section[i][j]);
+			}
+		}
+		return temp.stream().mapToInt(i -> i).toArray();
+	}
+
+	/**
+	 * @return a primitive 2D-array containing the sudoku board's numbers
+	 */
 	public int[][] getSudoku() {
 		return sudoku;
 	}
