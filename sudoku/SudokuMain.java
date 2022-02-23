e7de00c1bcf7636c2619a25d0a43ff33dead9da6
MisterMeldgaard <rameldgaard@gmail.com>
2022-02-23 10:45:43 +0100

    Update SudokuMain.java

--- sudoku/SudokuMain.java
+++ sudoku/SudokuMain.java
@@ -4,7 +4,7 @@ import java.io.File;
 public class SudokuMain {
 	
 	public static void main(String[] args) {
-		File file = new File("C:\\Users\\HP G1\\Documents\\GitHub\\Fagprojekt\\sudoku\\test.txt");
+		File file = new File("C:\\Users\\ramel\\OneDrive\\Dokumenter\\GitHub\\Fagprojekt\\sudoku\\test.txt");
 		SudokuModel model = new SudokuModel(file);
 		SudokuView view = new SudokuView(model);
 		SudokuController controller = new SudokuController(model, view);
