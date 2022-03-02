54d7ee5ccd8834f4a1d667be6ba0e940425ea93a
MisterMeldgaard <rameldgaard@gmail.com>
2022-03-02 09:50:17 +0100

    Added check for columns

--- sudoku/SudokuController.java
+++ sudoku/SudokuController.java
@@ -13,22 +13,26 @@ public class SudokuController {
 		this.view = view;
 	}
 	
+	//Simple method for checking validity of a sudoku
 	public static boolean checkValidity(int[][] sudoku){
 		boolean valid = new Boolean(true);
-		int[][] sortedGrid = new int[sudoku.length+1][sudoku.length+1];
+		//Grid for storing already found values
+		//int[][] sortedGrid = new int[sudoku.length+1][sudoku.length+1];
+		int[][] sortedGrid = new int[sudoku.length][sudoku.length];
+		//for(int i = sortedGrid.length-1; i >= 0; i--){
 		for(int i = sortedGrid.length-1; i >= 0; i--){
 			for(int k = 0; k < sortedGrid.length; k++){
 				sortedGrid[i][k] = 0;
 			}
 		}
 		
+		//Checking rows for duplicates
 		for(int i = 0; i < sudoku.length; i++){
 			for(int k = 0; k < sudoku.length; k++){
 				int cur = (sudoku[i][k]);
-				System.out.print(cur + " ");
 				if(cur != 0){
-					if(sortedGrid[i][cur] == 0){
-						sortedGrid[i][cur] = 1;
+					if(sortedGrid[i][cur-1] == 0){
+						sortedGrid[i][cur-1] = 1;
 					}
 					else{
 						valid = false;
@@ -36,22 +40,44 @@ public class SudokuController {
 				}
 				
 			}
-			System.out.println();
 		}
 
-		System.out.println();
-		for(int i = 0; i < sortedGrid.length; i++){
+		/*for(int i = 0; i < sortedGrid.length; i++){
 			for(int k = 0; k < sortedGrid.length; k++){
 				System.out.print(sortedGrid[i][k] + " ");
 			}
 			System.out.println();
 		}
+		System.out.println();*/
 
+		//Resetting the sorted grid
 		for(int i = sortedGrid.length-1; i >= 0; i--){
 			for(int k = 0; k < sortedGrid.length; k++){
 				sortedGrid[i][k] = 0;
 			}
 		}
+
+		//Checking columns for duplicates
+		for(int i = 0; i < sudoku.length; i++){
+			for(int k = 0; k < sudoku.length; k++){
+				int cur = (sudoku[k][i]);
+				if(cur != 0){
+					if(sortedGrid[i][cur-1] == 0){
+						sortedGrid[i][cur-1] = 1;
+					}
+					else{
+						valid = false;
+					}
+				}
+			}
+		}
+
+	//Resetting the sorted grid
+	for(int i = sortedGrid.length-1; i >= 0; i--){
+		for(int k = 0; k < sortedGrid.length; k++){
+			sortedGrid[i][k] = 0;
+		}
+	}
 
 		for(int r = 0; r < Math.sqrt(sudoku.length); r ++){
 			for(int c = 0; c < Math.sqrt(sudoku.length); c++){
@@ -59,8 +85,8 @@ public class SudokuController {
 					for(int bc = 0; bc < Math.sqrt(sudoku.length); bc ++){
 						int cur = sudoku[(c*3)+bc][(r*3)+br];
 						if(cur != 0){
-							if(sortedGrid[c+r*3][cur] == 0){
-								sortedGrid[c+r*3][cur] = 1;
+							if(sortedGrid[c+r*3][cur-1] == 0){
+								sortedGrid[c+r*3][cur-1] = 1;
 							}
 							else{
 								valid = false;
@@ -70,14 +96,13 @@ public class SudokuController {
 				}
 			}
 		}
-
-		System.out.println();
-		for(int i = 0; i < sortedGrid.length; i++){
+		/*for(int i = 0; i < sortedGrid.length; i++){
 			for(int k = 0; k < sortedGrid.length; k++){
 				System.out.print(sortedGrid[i][k] + " ");
 			}
 			System.out.println();
-		}
+		}*/
+
 		return valid;
 	}
 }
