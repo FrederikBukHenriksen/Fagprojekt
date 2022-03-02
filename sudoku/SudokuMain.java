32c0f4f139025bf9ebf91d859fda07028eb393d7
MisterMeldgaard <rameldgaard@gmail.com>
2022-03-02 14:51:02 +0100

    Merged changes into branch

--- sudoku/SudokuMain.java
+++ sudoku/SudokuMain.java
@@ -1,13 +1,7 @@
 package sudoku;
-import java.io.File;
 
 public class SudokuMain {
-	
 	public static void main(String[] args) {
-		File file = new File("C:\\Users\\ramel\\OneDrive\\Dokumenter\\GitHub\\Fagprojekt\\sudoku\\test.txt");
-		SudokuModel model = new SudokuModel(file);
-		SudokuView view = new SudokuView(model);
-		SudokuController controller = new SudokuController(model, view);
-		view.setVisible(view.f);
+		SudokuController controller = new SudokuController();
 	}
 }
