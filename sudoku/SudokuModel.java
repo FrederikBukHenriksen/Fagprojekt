831ecffac9a28a3027ed071fef0d0fab1df206d6
MisterMeldgaard <rameldgaard@gmail.com>
2022-03-02 15:14:11 +0100

    Fixed merging issues
    Merging issues due to too big differences were fixed by manually merging changes into main

--- sudoku/SudokuModel.java
+++ sudoku/SudokuModel.java
@@ -1,40 +1,101 @@
 package sudoku;
 
 import java.util.Scanner;
+import java.util.Stack;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class SudokuModel {
-
+	// Setting up variables
 	int[][] sudoku = new int[0][0];
+	Stack<int[][]> sudokuStack = new Stack<int[][]>();
+	int k = 0;
+	int n = 0;
 
-    public SudokuModel (File file){
-        Scanner scanner;
+	// constructor for the model
+	public SudokuModel() {
+		File file = new File("sudoku/test.txt");
+
+		Scanner scanner;
+		// reading the input
 		try {
 			scanner = new Scanner(file);
 			String setup = scanner.next();
-			int n = Character.getNumericValue(setup.charAt(0));
-			int k = Character.getNumericValue(setup.charAt(2));
+			Scanner setupScanner = new Scanner(setup);
+			setupScanner.useDelimiter(";");
+			// reading k & n
+			while (setupScanner.hasNext()) {
+				try {
+					String str = setupScanner.next();
+					k = Integer.parseInt(str);
+					str = setupScanner.next();
+					n = Integer.parseInt(str);
+				} catch (NumberFormatException ex) {
+					ex.printStackTrace();
+				}
+			}
+			setupScanner.close();
 
-			sudoku = new int[n*n][n*n];
-	    	for (int i = 0; i< n*n; i++) {
-	        String line = scanner.next();
-	    		for(int j = 0; j < n*n; j++) {
-	    			if(line.charAt(j*2)=='.') {
-	    				sudoku[i][j]=0;
-	    			} else {
-	    				sudoku[i][j]=Character.getNumericValue(line.charAt(j*2));
-	    			}
-	    		}
-	    	}
+			// Creating the board
+			sudoku = new int[n * k][n * k];
+			// Creating variables for looping through input
+			int c = 0;
+			int d = 0;
+			scanner.nextLine();
+			while (scanner.hasNextLine()) {
+				// Reads the next line
+				String line = scanner.nextLine();
+				Scanner lineScanner = new Scanner(line);
+				lineScanner.useDelimiter(";");
+				while (lineScanner.hasNext()) {
+					// Reads the next input on the line, separated by ";"
+					String str = lineScanner.next();
+					if (str.equals(".")) {
+						// If input is ".", convert to a "0"
+						sudoku[c][d] = 0;
+						// Go to next entry
+						d++;
+					} else {
+						try {
+							// If input isn't ".", read the number and insert into array
+							sudoku[c][d] = Integer.parseInt(str);
+						} catch (NumberFormatException ex) {
+							ex.printStackTrace();
+						}
+						// Go to next entry
+						d++;
+					}
+				}
+				// Go to next line, and start from first entry
+				c++;
+				d = 0;
+				lineScanner.close();
+			}
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
 
-	public int[][] getSudoku(){
+	public int[][] getSudoku() {
 		return sudoku;
+	}
+
+	public void setSudokuCell(int x, int y, int value) {
+		sudoku[x][y] = value;
+	}
+
+	public boolean isFilled() {
+		boolean result = true;
+		for (int i = 0; i < sudoku.length; i++) {
+			for (int j = 0; j < sudoku.length; j++) {
+				if (sudoku[i][j] == 0) {
+					result = false;
+				}
+			}
+		}
+		return result;
 	}
 
 }
--- sudoku/SudokuView.java
+++ sudoku/SudokuView.java
@@ -1,31 +1,240 @@
 package sudoku;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.lang.Math;
+
 import javax.swing.*;
 
 public class SudokuView {
-	
-	private SudokuModel model;
-	
-	public JFrame frame = new JFrame();
-	
-	public SudokuView(SudokuModel model) {
-		this.model = model;
+	public int test = 1;
+	public int xGrid = -1;
+	public int yGrid = -1;
 
-		JButton button = new JButton("Start");
-		button.setBounds(130,100,100,40);
-		frame.add(button);
-		frame.setSize(400,500);
-		frame.setLayout(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		if (SudokuController.checkValidity(model.sudoku)){
-			frame.setTitle("Valid puzzle");
-		}
-		else{
-			frame.setTitle("Invalid puzzle");
-		}
+	ArrayList<ArrayList<JToggleButton>> sudokuboardCells = new ArrayList();
+	ArrayList<JButton> numboardButtons = new ArrayList();
+
+	int[][] sudoku;
+
+	public JFrame f;
+
+	public SudokuView() {
+		f = new JFrame();
+		setVisible(f);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 	}
-	
+
+	public void showFrame(int[][] sudoku) {
+
+		JPanel mainGui = new JPanel(new GridLayout(1, 2, 50, 0));
+		JPanel panelGui = new JPanel(new GridLayout(3, 3, 10, 10));
+
+		createFields(sudoku);
+
+		for (int l = 0; l < 9; l++) {
+			JPanel panel = new JPanel(new GridLayout(3, 3));
+
+
+			for (int i = 0; i < 3; i++) {
+
+				for (int j = 0; j < 3; j++) {
+					if (sudoku[i + 3 * (l / 3)][(j + 3 * l) % 9] == 0) {
+						sudokuboardCells.get(i + 3 * (l / 3)).get((j + 3 * l) % 9)
+								.setFont(new Font("Serif", Font.PLAIN, 72));
+						panel.add(sudokuboardCells.get(i + 3 * (l / 3)).get((j + 3 * l) % 9));
+
+					} else {
+						sudokuboardCells.get(i + 3 * (l / 3)).get((j + 3 * l) % 9).setText(
+								String.valueOf(sudoku[i + 3 * (l / 3)][(j + 3 * l) % 9]));
+						sudokuboardCells.get(i + 3 * (l / 3)).get((j + 3 * l) % 9).setEnabled(false);
+						panel.add(sudokuboardCells.get(i + 3 * (l / 3)).get((j + 3 * l) % 9));
+					}
+				}
+			}
+			panelGui.add(panel);
+		}
+		mainGui.add(panelGui);
+		JPanel sideButtonGui = new JPanel(new GridLayout(2, 1, 0, 10));// creates buttons panels on the right side
+		JPanel specialButton = new JPanel(new GridLayout(1, 4, 0, 0));
+		JButton undo = new JButton("undo");
+		JButton remove = new JButton("remove");
+		JButton note = new JButton("note");
+		JButton newSudoku = new JButton("newSudoku");
+		ActionListener specialAction = new ActionListener() {
+			public void actionPerformed(ActionEvent actionEvent) {
+				AbstractButton abstractButton = (AbstractButton) actionEvent.getSource();
+				boolean selected = abstractButton.getModel().isSelected();
+				if (xGrid != -1) {
+					if (sudokuboardCells.get(xGrid).get(yGrid).isSelected()) { 
+						// deletes number from grid, if a cell is selected
+						// Does nothing if no cell is selected
+						sudokuboardCells.get(xGrid).get(yGrid).setText(null);
+						sudoku[xGrid][yGrid] = 0;
+					}
+
+				}
+			}
+		};
+		remove.addActionListener(specialAction);
+		specialButton.add(undo);
+		specialButton.add(remove);
+		specialButton.add(note);
+		specialButton.add(newSudoku);
+		sideButtonGui.add(specialButton);
+		JPanel buttonGui = new JPanel(new GridLayout(3, 3, 20, 20));// creates a 3/3 with numbers from 1-9
+		for (int j = 0; j < 9; j++) {
+			numboardButtons.add(new JButton(j + 1 + ""));// adds number as label to button
+			numboardButtons.get(j).setActionCommand(j + 1 + "");
+		}
+
+		for (int i = 1; i < 10; i++) {
+			test = i;
+			ActionListener actionListener = new ActionListener() {
+				public void actionPerformed(ActionEvent actionEvent) {
+					AbstractButton abstractButton = (AbstractButton) actionEvent.getSource();
+					boolean selected = abstractButton.getModel().isSelected();
+					if (xGrid != -1) {
+						if (sudokuboardCells.get(xGrid).get(yGrid).isSelected()) {
+							if (actionEvent.getActionCommand()
+									.equals(sudokuboardCells.get(xGrid).get(yGrid).getText())) {
+								// Check for what field is selected. Sets number to 0, if the same number was already selected
+								sudokuboardCells.get(xGrid).get(yGrid).setText(null);
+								sudoku[xGrid][yGrid] = 0;
+							} else {
+								sudokuboardCells.get(xGrid).get(yGrid).setText(actionEvent.getActionCommand());// check for if field is selected, sets cell to number
+								sudoku[xGrid][yGrid] = Integer.parseInt(actionEvent.getActionCommand());
+
+							}
+						}
+					}
+				}
+
+			};
+			numboardButtons.get(i - 1).addActionListener(actionListener);
+			numboardButtons.get(i - 1).setFont(new Font("Serif", Font.PLAIN, 72));
+			buttonGui.add(numboardButtons.get(i - 1));
+
+		}
+		sideButtonGui.add(buttonGui);
+		mainGui.add(sideButtonGui);
+
+		f.add(mainGui);
+		// setting grid layout of 3 rows and 3 columns
+		f.setSize(1000, 1000);
+	}
+
 	public void setVisible(JFrame frame) {
-		frame.setVisible(true);
+		f.setVisible(true);
 	}
-	
+
+	// Actionlisteners
+	void addNumboardListener(ActionListener listenForNumboardButtons) {
+		numboardButtons.forEach(b -> b.addActionListener(listenForNumboardButtons));
+	}
+
+	void addSudokuboardListener(ActionListener listenForSudokuboardButtons) {
+		int x = 0, y = 0; // Used to give the button an ActionCommand
+		for (ArrayList<JToggleButton> arraylist : sudokuboardCells) {
+			x++;
+			for (JToggleButton button : arraylist) {
+				y++;
+				button.setActionCommand(x + " " + y);
+				button.addActionListener(listenForSudokuboardButtons);
+			}
+		}
+	}
+
+
+	public void createFields(int[][] sudoku) {
+		for (int i = 0; i < 9; i++) {
+			ArrayList<JToggleButton> rows = new ArrayList();
+			for (int j = 0; j < 9; j++) {
+				rows.add(new JToggleButton(""));
+			}
+			sudokuboardCells.add(rows);
+		}
+	}
+
+	public void getSelected(JToggleButton buttonSelected) {
+		getButtons().forEach(b -> b.setSelected(false));
+		buttonSelected.setSelected(true);
+
+	}
+
+	public ArrayList<JToggleButton> getButtons() {
+		ArrayList<JToggleButton> buttonsArray = new ArrayList<>();
+
+		for (ArrayList<JToggleButton> arraylist : sudokuboardCells) {
+			for (JToggleButton button : arraylist) {
+				buttonsArray.add(button);
+			}
+		}
+		return buttonsArray;
+	}
+
+	public JToggleButton isSelected() { // TODO: ret til abstractbutton
+
+		ArrayList<JToggleButton> result = (ArrayList<JToggleButton>) getButtons().stream()
+				.filter(b -> b.isSelected())
+				.collect(Collectors.toList());
+		return result.get(0);
+
+	}
+
+	public int[] getCellCoordinate(JToggleButton selected) {
+		int[] coordinate = new int[2];
+
+		for (int x = 0; x < 9; x++) {
+			for (int y = 0; y < 9; y++) {
+				JToggleButton button = sudokuboardCells.get(x).get(y);
+				if (button.equals(selected)) {
+					coordinate[0] = x;
+					coordinate[1] = y;
+				}
+			}
+		}
+		return coordinate;
+	}
+
+	public void updateBoard(int[][] sudoku) {
+
+		// Run through all the cells and assign text
+		for (int x = 0; x < sudoku.length; x++) {
+			for (int y = 0; y < sudoku.length; y++) {
+				if (sudoku[x][y] != 0) {
+					JToggleButton button = sudokuboardCells.get(x).get(y);
+					button.setText(String.valueOf(sudoku[x][y]));
+				}
+			}
+		}
+	}
+
+	public void setTitle(String string) {
+		f.setTitle(string);
+	}
+
+	public void updateFrameTitle(boolean checkValidity, boolean isFilled) {
+		if (checkValidity) {
+			if (isFilled) {
+				f.setTitle("Filled and valid");
+			} else {
+				f.setTitle("Valid");
+			}
+		} else {
+			if (isFilled) {
+				f.setTitle("Filled and invalid");
+			} else {
+				f.setTitle("Invalid");
+			}
+
+		}
+	}
+
 }
