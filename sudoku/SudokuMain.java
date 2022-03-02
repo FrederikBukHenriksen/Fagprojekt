4b83ee58448ce0499980f8a1d7912a4388b7f2b2
MisterMeldgaard <rameldgaard@gmail.com>
2022-03-02 12:47:06 +0100

    Made file reading dynamic

--- sudoku/SudokuMain.java
+++ sudoku/SudokuMain.java
@@ -4,7 +4,7 @@ import java.io.File;
 public class SudokuMain {
 	
 	public static void main(String[] args) {
-		File file = new File("C:\\Users\\ramel\\OneDrive\\Dokumenter\\GitHub\\Fagprojekt\\sudoku\\test.txt");
+		File file = new File("C:\\Users\\ramel\\OneDrive\\Dokumenter\\GitHub\\Fagprojekt\\sudoku\\testing.txt");
 		SudokuModel model = new SudokuModel(file);
 		SudokuView view = new SudokuView(model);
 		SudokuController controller = new SudokuController(model, view);
--- sudoku/SudokuModel.java
+++ sudoku/SudokuModel.java
@@ -12,14 +12,28 @@ public class SudokuModel {
 	
     public SudokuModel (File file){
         Scanner scanner;
+		int k = 0;
+		int n = 0;
 		try {
 			scanner = new Scanner(file);
 			String setup = scanner.next();
-			int n = Character.getNumericValue(setup.charAt(0));
-			int k = Character.getNumericValue(setup.charAt(2));
+			Scanner setupScanner = new Scanner(setup);
+			setupScanner.useDelimiter(";");
+			while(setupScanner.hasNext()){
+				try{
+					String str = setupScanner.next();
+					k = Integer.parseInt(str);
+					str = setupScanner.next();
+					n = Integer.parseInt(str);
+				}
+				catch (NumberFormatException ex){
+					ex.printStackTrace();
+				}
+			}
+			setupScanner.close();
 
-			sudoku = new int[n*n][n*n];
-	    	for (int i = 0; i< n*n; i++) {
+			sudoku = new int[n*k][n*k];
+	    	/*for (int i = 0; i< n*n; i++) {
 	        String line = scanner.next();
 	    		for(int j = 0; j < n*n; j++) {
 	    			if(line.charAt(j*2)=='.') {
@@ -28,7 +42,55 @@ public class SudokuModel {
 	    				sudoku[i][j]=Character.getNumericValue(line.charAt(j*2));
 	    			}
 	    		}
-	    	}
+	    	}*/
+			int c = 0;
+			int d = 0;
+			scanner.nextLine();
+			while(scanner.hasNextLine()){
+				String line = scanner.nextLine();
+
+				Scanner lineScanner = new Scanner(line);
+				lineScanner.useDelimiter(";");
+				while(lineScanner.hasNext()){
+					String str = lineScanner.next();
+					if(str.equals(".")){
+						//System.out.println("c: " + c);
+						sudoku[c][d] = 0;
+						//System.out.println("i: " + d + ", j: " + c);
+						d++;
+						/*for(int a = 0; a < sudoku.length; a++){
+							for(int b = 0; b < sudoku.length; b++){
+								System.out.print(sudoku[a][b] + " ");
+							}
+							System.out.println();
+						}
+						System.out.println();*/
+					} 
+					else{
+						try{
+							sudoku[c][d] = Integer.parseInt(str);
+							/*System.out.println("i: " + d + ", j: " + c);
+							for(int a = 0; a < sudoku.length; a++){
+								for(int b = 0; b < sudoku.length; b++){
+									System.out.print(sudoku[a][b] + " ");
+								}
+								System.out.println();
+							}
+							System.out.println();*/
+						}
+						catch (NumberFormatException ex){
+							ex.printStackTrace();
+						}
+						d++;
+					}
+				}
+				c++;
+				d = 0;
+				lineScanner.close();
+			}
+			
+
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
