ed5ee8792137fd0123603a85e5f14234a007cdba
MisterMeldgaard <rameldgaard@gmail.com>
2022-06-03 12:19:36 +0200

    Update SudokuModel.java

--- sudoku/SudokuModel.java
+++ sudoku/SudokuModel.java
@@ -38,7 +38,7 @@ public class SudokuModel {
 	// constructor for the model
 	public SudokuModel(SudokuView view) {
 		this.view = view;
-		File file = new File("sudoku/Puzzles_1/Puzzle_S_000.dat");
+		File file = new File("sudoku/Puzzles_1/Puzzle_3_01.dat");
 
 		Scanner scanner;
 		// reading the input
