9bbbc40444e96a9c5fcf98985a7ea90b749588ee
FrederikBukHenriksen <62750296+FrederikBukHenriksen@users.noreply.github.com>
2022-06-14 09:14:42 +0200

    update

--- sudoku/Controller/MarkCells/ClassicSudokuMarkCells.java
+++ sudoku/Controller/MarkCells/ClassicSudokuMarkCells.java
@@ -1,5 +1,21 @@
 package sudoku.Controller.MarkCells;
 
-public class ClassicSudokuMarkCells {
+public class ClassicSudokuMarkCells implements MarkCellsInterface {
+
+    public void markCells() {
+        peersHorisontal();
+        peersVertical();
+        peersSquare();
+    }
+
+    protected void peersSquare() {
+    }
+
+    protected void peersVertical() {
+    }
+
+    protected void peersHorisontal() {
+
+    }
 
 }
